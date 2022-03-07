25e8a4a2c36dd61fc5d28b97454a6cf28116211f
Chansith <78367890+Chansith@users.noreply.github.com>
2022-03-08 00:22:13 +0700

    commit1

--- api.js
+++ api.js
@@ -23,17 +23,15 @@ client.connect(function(err) {
     console.log('Connected to the MySQL server.');
   });
 
-// console.log(client);
+
 app.post("/member/Register", (req, res) =>{
     const username = req.body.username;
     const password = req.body.password;
-    // console.log(res);
-    // console.log(req);
-    // `SELECT COUNT(id) FROM public."users"`
+  
     client.query(`SELECT * FROM new_schema.client WHERE username ='${username}'`,
     (err, result) =>{
         console.log(err);
-        // console.log(result);
+        
         if (result.length == 0) {          
             client.query(`INSERT INTO client (username, password) VALUES('${username}','${password}')`,
             [username, password],
@@ -68,7 +66,7 @@ app.post('/member/login',(req, res)=>{
             if (err) {
                 res.send({ err: err });
             } 
-            // console.log(result.length);
+            
            
                 if(result.length == 0){
                     res.send({message:"Wrong username or password combination"});
@@ -105,7 +103,7 @@ app.post('/products', async (req, res)=>{
             if (err) {
                 console.log({ err: err });
             } 
-            // console.log(result.length);
+            
             
                     console.log(result); 
 
@@ -114,7 +112,7 @@ app.post('/products', async (req, res)=>{
             
         
         );
-    ///ใส่?ที่109,105เช็คค่าว่ามีอะเป่า
+   
 });
 
 app.get('/products',(req, res)=>{
@@ -126,7 +124,7 @@ app.get('/products',(req, res)=>{
             if (err) {
                 res.send({ err: err });
             } 
-            // console.log(result.length);
+            
            
                 if(result.length == 0){
                     res.send({message:"Wrong username or password combination"});
@@ -143,120 +141,3 @@ app.get('/products',(req, res)=>{
 app.listen(3001, ()=>{
     console.log("Sever is now listening at port 3001")
 })
-
-
-// client.query("SELECT * FROM client WHERE username = ?",
-// [username],
-// (err, result) =>{
-//     if (result.rows.length == 0) {          
-//         client.query("INSERT INTO client (username, password) VALUES(?.?)",
-//         [username, password],
-//             (err, result) =>{
-//                 console.log(err);
-//             }
-//             );
-        
-//         ; 
-//     }
-//     else{
-//         res.send({message:"user already register"});
-//     }
-// }
-// );
-
-
-
-
-
-// client.query(`SELECT * FROM public."users" WHERE username = '${username}'`,
-// (err, result) =>{
-//     if (result.rows.length == 0) {
-//         client.query(`SELECT COUNT(id) FROM public."users"`,
-//         (err, result) =>{
-//             console.log(result.rows[0].count); 
-//             client.query(`INSERT INTO public."users" (id, username, password) VALUES(${result.rows[0].count}, '${username}','${password}')`,
-//             (err, result) =>{
-//                 console.log(err);
-//             }
-//             );
-//         }
-//         ); 
-//     }
-//     else{
-//         res.send({message:"user already register"});
-//     }
-// }
-// );
-
-
-
-// }
-// );
-
-// app.post('/login',(req, res)=>{
-// const username = req.body.username;
-// const password = req.body.password;
-
-
-// client.query(
-//     `SELECT * FROM public."users" WHERE username = '${username}' AND password = '${password}'`,
-    
-//     (err, result) =>{
-//         if (err) {
-//             res.send({ err: err });
-//         } 
-//         console.log(result.rows.length);
-       
-//             if(result.rows.length == 0){
-//                 res.send({message:"Wrong username or password combination"});
-//             }else{
-//                 res.send(result);
-//             }
-        
-//         }
-    
-// );
-// });
-
-
-
-
-
-
-
-// (err, result) =>{
-//     if (err) {
-//         res.send({ err: err });
-//     } 
-//     console.log(result.rows.length);
-//     bcrypt.conpare(password,result[0].password,(error, response) => {
-//         if(result.rows.length == 0){
-//             res.send({message:"Wrong username or password combination"});
-//         }else{
-//             res.send(result);
-//         }
-//     });
-//     }
-
-// app.get('/users', (req, res)=>{
-    
-//     client.query(`Select * from users`, (err, result)=>{
-//         console.log(result)
-//         if(!err){
-//             res.send(result.rows);
-//         }
-//     });
-//     client.end;
-// })
-
-// app.get('/users/:id', (req, res)=>{
-//     client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
-//         if(!err){
-//             res.send(result.rows);
-//         }
-//     });
-//     client.end;
-// })
-
-
-
